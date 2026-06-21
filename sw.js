c1030254f6d36883e929a33dd6d19374f9622c05
Nour-Al-Hazzouri <Nour-Al-Hazzouri@users.noreply.github.com>
2026-06-21 13:57:24 +0000

    deploy: eea007d175ad8d9c8d7bc1d8f99f46121eae7f41

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE_NAME="jezdan-cache-v1",ASSETS_TO_CACHE=["./","./index.html","./main.js","./manifest.json"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(e=>e.addAll(ASSETS_TO_CACHE)))}),self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(t=>t||fetch(e.request)))});
